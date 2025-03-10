46f3932bbb8780a6b232d321783de0ca3fc35a4c
Marine Lancien (Hexagone IT) <marine.lancien@hexagone-it.fr>
2025-03-10 12:36:01 +0100

    fix: register AppController and its dependencies in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,19 +4,26 @@ import { ThrottlerModule } from '@nestjs/throttler';
 import { AuthModule } from './auth/auth.module';
 import { WaterIntakeModule } from './water-intake/water-intake.module';
 import { UsersModule } from './users/users.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    ThrottlerModule.forRoot([{
-      ttl: 60000,
-      limit: 100,
-    }]),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
     AuthModule,
     WaterIntakeModule,
     UsersModule,
   ],
+  controllers: [AppController],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
